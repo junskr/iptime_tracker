d4d8139efd967811e33e05f1f260275e9621d28b
juns <35132274+junskr@users.noreply.github.com>
2021-11-24 23:13:37 +0900

    Update to support mesh networks

--- custom_components/iptime_tracker/const.py
+++ custom_components/iptime_tracker/const.py
@@ -1,3 +1,5 @@
+DOMAIN = 'iptime_tracker'
+
 CONF_URL = 'iptime_url'
 CONF_ID = 'iptime_id'
 CONF_PASSWORD = 'iptime_pw'
@@ -6,4 +8,16 @@ CONF_NAME = 'name'
 CONF_MAC = 'mac'
 DEFAULT_INTERVAL = 5
 
-DOMAIN = 'iptime_tracker'
+HOSTINFO_URN = '/login/hostinfo2.cgi'
+LOGIN_URN = '/sess-bin/login_handler.cgi'
+LOGOUT_URN = '/sess-bin/login_session.cgi?logout=1'
+WLAN_2G_URN = '/sess-bin/timepro.cgi?tmenu=iframe&smenu=macauth_pcinfo_status&bssidx=0'
+WLAN_5G_URN = '/sess-bin/timepro.cgi?tmenu=iframe&smenu=macauth_pcinfo_status&bssidx=65536'
+MESH_URN = '/sess-bin/timepro.cgi?tmenu=wirelessconf&smenu=easymesh'
+M_LOGIN_URN = '/m_handler.cgi'
+M_LOGOUT_URN = '/m_login.cgi?logout=1'
+M_WLAN_2G_URN = '/cgi/iux_get.cgi?tmenu=wirelessconf&smenu=macauth&act=status&wlmode=2g&bssidx=0'
+M_WLAN_5G_URN = '/cgi/iux_get.cgi?tmenu=wirelessconf&smenu=macauth&act=status&wlmode=5g&bssidx=65536'
+M_MESH_URN = '/cgi/iux_get.cgi?tmenu=sysconf&smenu=info&act=status'
+MESH_STATION_URN = '/easymesh/api.cgi?key=topology'
+TIME_OUT = 5
